2c7778044e35448a2b7bffb2995c7c6336437de3
Shemeeraps <120811598+Shemeeraps@users.noreply.github.com>
2023-01-13 16:51:10 +0530

    Delete inmakes directory

--- inmakes/django_static_templates/demoapp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Place,Actor
-# Register your models here.
-admin.site.register(Place)
-admin.site.register(Actor)
--- inmakes/django_static_templates/demoapp/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Place,Actor
-# Create your views here.
-def index(request):
-    obj1=Place.objects.all()
-    obj2=Actor.objects.all()
-    return render(request,'myindex.html',{'result1':obj1,'result2':obj2})
